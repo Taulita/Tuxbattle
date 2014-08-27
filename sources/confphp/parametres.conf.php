c51a9b824259c73d344d8a38b2e94ea9ab5de804
Taulita <dev@pc5.home>
2014-08-27 10:00:11 +0200

    This is the end

--- sources/confphp/parametres.conf.php
+++ sources/confphp/parametres.conf.php
@@ -2,7 +2,7 @@
 	$server = "localhost";
 	$db_name = "tuxbattle";
 	$user = "root";
-	$password = "troiswa";
+	$password = "root";
 
 
 	$db = mysqli_connect($server, $user, $password, $db_name) or die ('<h1 style="font-family: sans-serif; font-size: 16pt; text-align: center; border: 4px solid #FF0000; padding: 16px; width: 400; margin: 0 auto; border-radius: 32px; background-color: #FFC0C0; margin-top: 128px; color: #FF0000;">Erreur : Connexion &agrave; la base de donn&eacute;es impossible</h1>');
